2c576151c97fef499909926a5f8ad0c264d12e7b
jefflangtech <jeff.m.lang@gmail.com>
2023-04-13 20:13:09 -0700

    Did basic set up for using a factory function to initialize the dropdown menus

--- resources/js/index.js
+++ resources/js/index.js
@@ -56,12 +56,48 @@ const themeControls = (function() {
   let fontElements;
   let fontControls;
   let fontLabel;
+  let dropDownMenus = [];
   let themeElements;
 
   function init() {
     fontElements = document.querySelectorAll('.font-primary');
     fontControls = document.querySelectorAll('[data-font]');
     fontLabel = document.getElementById('font-select-label').childNodes[0];
+
+    // Locate all menu containers
+    let menus = document.querySelectorAll('.dropdown-menu');
+    menus.forEach(element => {
+      let menuHeader;
+      let menuContents;
+      let childElements = element.children;
+
+      // Search the children for the header and contents elements
+      for(let i = 0; i < childElements.length; i++) {
+        let childClasses = childElements[i].classList;
+        for(let j = 0; j < childClasses.length; j++) {
+          if(childClasses[j] === "dropdown-menu-header") {
+            menuHeader = childElements[i];
+          }
+          if(childClasses[j] === "dropdown-menu-contents") {
+            menuContents = childElements[i];
+          };
+        }
+      }
+      // Create a new menu object and push it on to the list of page menus
+      dropDownMenus.push(new Menu(element, menuHeader, menuContents));
+    });
+    console.dir(dropDownMenus[0]);
+  };
+
+  // Create a menu object for each dropdown menu element
+  function Menu(element, header, contents) {
+    this.element = element;
+    this.header = header;
+    this.contents = contents;
+  };
+
+  Menu.prototype.testFunc = function() {
+    return this.name;
   };
 
   function getFontControls() {
