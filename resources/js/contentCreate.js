43a09c6d480cea34bb5365070b051f020b5098c3
jefflangtech <jeff.m.lang@gmail.com>
2023-04-30 08:43:08 -0700

    Moved the theme controls and element creation code into separate modules

---
+++ resources/js/contentCreate.js
@@ -0,0 +1,136 @@
+import themeControls from "./themeControls.js";
+
+// Begin module for creating HTML layouts & content
+const contentCreate = (() => {
+
+  const dictionaryEntry = document.querySelector('.dictionary-entry');
+  const footer = document.querySelector('footer');
+
+  function parse(data) {
+
+    console.dir(data.data);
+
+    if(dictionaryEntry.innerHTML) {
+      dictionaryEntry.innerHTML = '';
+      if(footer.innerHTML) {
+        footer.innerHTML = '';
+        footer.classList.remove('footer-style');
+      }
+    }
+
+    if(!data.success) {
+      errorContent(data.data);
+    } else {
+      wordContainer(data.data);
+      definitionContainers(data.data);
+      footerContent(data.data);
+      themeControls.loadFontElements();
+      themeControls.loadThemeElements();
+      themeControls.loadThemeState();
+    }
+
+  };
+
+  function errorContent(data) {
+
+    dictionaryEntry.innerHTML = '<div id="error-container" class="font-primary"><p id="emoji">&#x1F615;</p><h3 id="error-title"></h3><p id="error-message" class="medium"></p></div>';
+
+    const errorTitle = document.getElementById('error-title');
+    const errorMessage = document.getElementById('error-message');
+
+    errorTitle.innerHTML = data.title;
+    errorMessage.innerHTML = `${data.message} ${data.resolution}`;
+
+  };
+
+  function definitionContainers(data) {
+
+    for(let meanings of data.meanings) {
+
+      let defContainerEl = document.createElement('div');
+      defContainerEl.setAttribute('class', 'definition-container');
+      defContainerEl.innerHTML = `<h2 class="language-element font-primary font-secondary bg-theme-color">${meanings.partOfSpeech}</h2><h3 class="part-of-speech font-primary">Meaning</h3><ul></ul>`;
+
+      for(let def of meanings.definitions) {
+        let defEl = document.createElement('li');
+        defEl.innerHTML = `<p class="medium font-primary">${def.definition}</p>`;
+
+        if(def.example) {
+          let exEl = document.createElement('p');
+          exEl.setAttribute('class', 'medium example font-primary');
+          exEl.innerHTML = `"${def.example}"`;
+          defEl.appendChild(exEl);
+        }
+
+        defContainerEl.children[2].appendChild(defEl);
+
+      }
+
+      dictionaryEntry.appendChild(defContainerEl);
+
+    }
+
+  }
+
+  function wordContainer(data) {
+
+    dictionaryEntry.innerHTML = '<div class="word-container"><div class="word-container-child"><h1 id="word" class="font-primary"></h1><p id="phonetics"></p></div><div class="word-container-child"><svg id="play-button" xmlns="http://www.w3.org/2000/svg" width="75" height="75" viewBox="0 0 75 75"><g fill="#A445ED" fill-rule="evenodd"><circle cx="37.5" cy="37.5" r="37.5" opacity=".25"/><path d="M29 27v21l21-10.5z"/></g></svg></div><audio id="audio-player"><source id="audio-source" src="" type="audio/mpeg">Your browser does not support the audio element.</audio></div>';
+
+    const wordEl = document.getElementById('word');
+    const phoneticsEl = document.getElementById('phonetics');
+    const audio = document.getElementById('audio-source');
+    const playButton = document.getElementById('play-button');
+    let audioBoolean = false;
+
+    wordEl.innerHTML = data.word;
+    
+    if(data.phonetic) {
+      phoneticsEl.innerHTML = data.phonetic;
+    }
+    for(let item of data.phonetics) {
+      if(!phoneticsEl.innerHTML && item.text) {
+        phoneticsEl.innerHTML = item.text;
+      }
+      if(!audioBoolean && item.audio) {
+        audio.setAttribute('src', item.audio);
+        audioBoolean = true;
+
+        playButton.addEventListener('click', function() {
+          const audioPlayer = document.getElementById('audio-player');
+    
+          if(audioPlayer.paused) {
+            audioPlayer.play();
+          } else {
+            audioPlayer.pause();
+          }
+    
+        });
+      }
+    }
+    if(!audioBoolean) {
+      playButton.classList.add('hidden');
+    } else {
+      playButton.classList.remove('hidden');
+    }
+
+  };
+
+  function footerContent(data) {
+    
+    footer.classList.add('footer-style');
+    footer.innerHTML = '<span class="link-wrapper"><p class="small font-primary">Source</p></span><p class="small"><span class="link-wrapper"><a id="source-url" class="footer-link font-primary bg-theme-color" href="" target="_blank"></a></span><span class="svg-wrapper"><svg class="new-window-icon" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14"><path fill="none" stroke="#838383" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M6.09 3.545H2.456A1.455 1.455 0 0 0 1 5v6.545A1.455 1.455 0 0 0 2.455 13H9a1.455 1.455 0 0 0 1.455-1.455V7.91m-5.091.727 7.272-7.272m0 0H9m3.636 0V5"/></svg></span></p>';
+
+    const source = document.getElementById('source-url');
+    
+    source.setAttribute('href', data.sourceUrls[0]);
+    source.innerHTML = data.sourceUrls[0];
+
+  };
+
+  return {
+    parse
+  };
+
+})();
+
+export default contentCreate;
---
+++ resources/js/themeControls.js
@@ -0,0 +1,273 @@
+// themeControl initializes all the data and functions necessary to operate
+// the theme and font changing controls
+const themeControls = (function() {
+
+  // Elements required for user selected fonts
+  let primaryFontElements;
+  let secondaryFontElements;
+  let fontControls;
+  let fontLabel;
+
+  // Elements required for menu controls
+  let dropDownMenus = [];
+
+  // Elements required for light-dark mode
+  let colorThemeActive = false;
+  let toggleSwitches = [];
+  let themeElements = {};
+
+  // Capture any necessary variables declared in CSS :root
+  const rootStyle = getComputedStyle(document.documentElement);
+  const transitionTiming = rootStyle.getPropertyValue('--transition-delay').slice(0, -1);
+
+  function init() {
+
+    loadFontElements();
+
+    // Locate all menu containers
+    // -----------------------------------------------------
+    let menus = document.querySelectorAll('.dropdown-menu');
+    menus.forEach(element => {
+      let menuHeader;
+      let menuContents;
+      let childElements = element.children;
+
+      // Search the children for the header and contents elements
+      for(let i = 0; i < childElements.length; i++) {
+        let childClasses = childElements[i].classList;
+        for(let j = 0; j < childClasses.length; j++) {
+          if(childClasses[j] === "dropdown-menu-header") {
+            menuHeader = childElements[i];
+          };
+          if(childClasses[j] === "dropdown-menu-contents") {
+            menuContents = childElements[i];
+          };
+        };
+      };
+      // Create a new menu object and push it on to the list of page menus
+      dropDownMenus.push(new Menu(element, menuHeader, menuContents));
+    });
+    // -----------------------------------------------------
+    
+    // Locate all toggle switches
+    // -----------------------------------------------------
+    let toggles = document.querySelectorAll('.toggle');
+    toggles.forEach(element => {
+      let toggleButton;
+      let childElements = element.children;
+      let toggleContainer = element.parentElement;
+      let svgElement = element.nextElementSibling;
+      for(let i = 0; i < childElements.length; i++) {
+        let childClasses = childElements[i].classList;
+        for(let j = 0; j < childClasses.length; j++) {
+          if(childClasses[j] === "toggle-switch") {
+            toggleButton = childElements[i];
+          };
+        };
+      };
+
+      // Create a new toggleSwitch object and push it on to the list
+      toggleSwitches.push(new ToggleControl(toggleContainer, element, toggleButton, svgElement));
+    });    
+    // -----------------------------------------------------
+
+    // Locate all color theme elements
+    loadThemeElements();
+
+    // Initialize event listeners
+    fontControls.forEach(element => {
+      element.addEventListener('click', function() {
+        changeStyle(element);
+      });
+    });
+
+    toggleSwitches.forEach(item => {
+      item.parent.addEventListener('click', function() {
+        item.switchToggle();
+      })
+    });
+
+    dropDownMenus.forEach(menuContainer => {
+      menuContainer.element.addEventListener('click', function() {
+        showFontMenu(menuContainer.contents);
+      });
+      menuContainer.element.addEventListener('mouseleave', function() {
+        hideFontMenu(menuContainer.contents);
+      });
+      menuContainer.contents.addEventListener('mouseleave', function() {
+        hideFontMenu(menuContainer.contents);
+      });
+      menuContainer.element.addEventListener('mouseenter', function() {
+        stopTimeout(menuContainer.contents);
+      });
+      menuContainer.contents.addEventListener('mouseenter', function() {
+        stopTimeout(menuContainer.contents);
+      });
+
+    });
+
+    loadThemeState();
+    // End of init() call
+
+  };
+
+
+  // Create a menu object for each dropdown menu element
+  function Menu(element, header, contents) {
+    this.element = element;
+    this.header = header;
+    this.contents = contents;
+  };
+
+  function ToggleControl(parent, element, toggleButton, svgElement) {
+    this.parent = parent;
+    this.element = element;
+    this.toggleButton = toggleButton;
+    this.svg = svgElement;
+  };
+
+  ToggleControl.prototype.switchToggle = function() {
+    let style = this.toggleButton.style.transform;
+    if(!style) {
+      colorThemeActive = true;
+      this.element.style.backgroundColor = 'var(--active-lavender)';
+      this.svg.childNodes[0].style.stroke = 'var(--active-lavender)';
+      this.toggleButton.style.transform = 'translateX(125%)';
+    } else {
+      colorThemeActive = false;
+      this.element.style.backgroundColor = '';
+      this.svg.childNodes[0].style.stroke = '';
+      this.toggleButton.style.transform = '';
+    }
+    loadThemeState();
+  };
+
+  function loadFontElements() {
+    primaryFontElements = document.querySelectorAll('.font-primary');
+    secondaryFontElements = document.querySelectorAll('.font-secondary');
+    fontControls = document.querySelectorAll('[data-font]');
+    fontLabel = document.getElementById('font-select-label').childNodes[0];
+  };
+
+  function loadThemeElements() {
+    themeElements.background = [];
+    themeElements.boxShadow = [];
+    themeElements.textInput = [];
+
+    document.querySelectorAll('.bg-theme-color').forEach(element => {
+      themeElements.background.push(element);
+    });
+    document.querySelectorAll('.box-shadow').forEach(element => {
+      themeElements.boxShadow.push(element);
+    });
+    document.querySelectorAll('input[type="text"]').forEach(element => {
+      themeElements.textInput.push(element);
+    });
+
+  };
+
+  function stopTimeout(elem) {
+    if(elem.timeoutId) {
+      clearTimeout(elem.timeoutId);
+    }
+  };
+
+  function toggleHide(elem) {
+    elem.classList.toggle('hidden');
+  };
+
+  function showFontMenu(contents) {
+    for(let i = 0; i < contents.classList.length; i++) {
+      if(contents.classList[i] === 'hidden') {
+        toggleHide(contents);
+        return;
+      }
+    }
+  };
+
+  function hideFontMenu(contents) {
+    if(contents.classList.length > 0) {
+      for(let i = 0; i < contents.classList.length; i++) {
+        if(contents.classList[i] === 'hidden') {
+          return;
+        }
+      }
+    }
+    if(contents.timeoutId) {
+      clearTimeout(contents.timeoutId);
+    }
+    contents.timeoutId = setTimeout(() => {
+      toggleHide(contents);
+    }, parseFloat(transitionTiming) * 1000);
+  };
+
+  function loadThemeState() {
+    if(colorThemeActive) {
+      themeElements.background.forEach(element => {
+        element.classList.add('bg-dark');
+      });
+      themeElements.boxShadow.forEach(element => {
+        element.classList.add('box-shadow-dark');
+      });
+      themeElements.textInput.forEach(element => {
+        element.classList.add('text-input-dark');
+      });
+    } else {
+      themeElements.background.forEach(element => {
+        element.classList.remove('bg-dark');
+      });
+      themeElements.boxShadow.forEach(element => {
+        element.classList.remove('box-shadow-dark');
+      });
+      themeElements.textInput.forEach(element => {
+        element.classList.remove('text-input-dark');
+      });
+    }
+
+  };
+
+  function changeStyle(fontSelector) {
+    let font = fontSelector.dataset.font
+    primaryFontElements.forEach(element => {
+      element.style.fontFamily = `${font}`;
+    });
+    
+    // Specific style changes for the language elements based on font selected
+    switch(font) {
+      case 'var(--sans-serif)':
+        secondaryFontElements.forEach(element => {
+          element.style.fontWeight = '700';
+          element.style.transform = 'translateY(-50%) skew(-10deg)';
+        });
+        break;
+      case 'var(--serif)':
+        secondaryFontElements.forEach(element => {
+          element.style.fontWeight = '400';
+          element.style.transform = 'translateY(-50%) skew(0deg)';
+        });
+        break;
+      case 'var(--mono)':
+        secondaryFontElements.forEach(element => {
+          element.style.fontWeight = '700';
+          element.style.transform = 'translateY(-50%) skew(0deg)';
+        });
+        break;
+      default:
+        console.log('Error selected font for secondary font elements');
+    };
+
+    fontLabel.textContent = fontSelector.innerHTML;
+  };
+
+  // Initialize the state and functionality of the UI
+  init();
+
+  return {
+    loadFontElements,
+    loadThemeElements,
+    loadThemeState
+  };
+
+})();
+
+export default themeControls;
